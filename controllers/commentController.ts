a6fe5ba38c819f463a503597fd63fb5b4592e4fb
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-12-11 22:34:26 +0100

    The deleteComment method has been created.

--- controllers/commentController.ts
+++ controllers/commentController.ts
@@ -1,6 +1,8 @@
 import {IGetUserAuthInfoRequest} from "../types/userAuthResponseInterface";
 import {NextFunction, Response} from "express";
 import Comment from "../models/Comment";
+import Video from "../models/Video";
+import {createError} from "../utils/error";
 
 export const addComment = async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
     const newComment = new Comment({...req.body, userId: req.user.id});
@@ -14,7 +16,14 @@ export const addComment = async (req: IGetUserAuthInfoRequest, res: Response, ne
 
 export const deleteComment = async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
     try {
-
+        const comment = await Comment.findById(req.params.id);
+        const video = await Video.findById(req.params.id);
+        if(req.user.id === comment.userId || req.user.id === video.userId) {
+            await Comment.findByIdAndDelete(req.params.id);
+            res.status(200).json('The comment has been deleted.');
+        } else {
+            return next(createError(403, 'You can delete only your comment!'));
+        }
     } catch (err) {
         next(err);
     }
